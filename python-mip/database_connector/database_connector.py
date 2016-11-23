79d7868ecc86cdea1f4b94505922f8dad6064536
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-11-23 15:03:33 +0100

    use env vars for DB config

--- python-mip/database_connector/database_connector.py
+++ python-mip/database_connector/database_connector.py
@@ -1,74 +1,13 @@
 #!/usr/bin/env python3
 
 import psycopg2
-
-
-# Default values for databases configuration
-
-science_db_host = '172.17.0.1'
-science_db_port = '65432'
-science_db_name = 'science'
-science_db_user = 'science'
-science_db_password = 'sciencepass'
-
-meta_db_host = '172.17.0.1'
-meta_db_port = '65432'
-meta_db_name = 'meta'
-meta_db_user = 'meta'
-meta_db_password = 'metapass'
-
-analytics_db_host = '172.17.0.1'
-analytics_db_port = '65432'
-analytics_db_name = 'analytics'
-analytics_db_user = 'analytics'
-analytics_db_password = 'analyticspass'
-
-
-def setup_science_db(host, port, name, user, password):
-    global science_db_host
-    global science_db_port
-    global science_db_name
-    global science_db_user
-    global science_db_password
-
-    science_db_host = host
-    science_db_port = port
-    science_db_name = name
-    science_db_user = user
-    science_db_password = password
-
-
-def setup_meta_db(host, port, name, user, password):
-    global meta_db_host
-    global meta_db_port
-    global meta_db_name
-    global meta_db_user
-    global meta_db_password
-
-    meta_db_host = host
-    meta_db_port = port
-    meta_db_name = name
-    meta_db_user = user
-    meta_db_password = password
-
-
-def setup_analytics_db(host, port, name, user, password):
-    global analytics_db_host
-    global analytics_db_port
-    global analytics_db_name
-    global analytics_db_user
-    global analytics_db_password
-
-    analytics_db_host = host
-    analytics_db_port = port
-    analytics_db_name = name
-    analytics_db_user = user
-    analytics_db_password = password
+import os
 
 
 def get_vars_metadata():
-    conn = psycopg2.connect(host=meta_db_host, port=meta_db_port, dbname=meta_db_name, user=meta_db_user,
-                            password=meta_db_password)
+    conn = psycopg2.connect(host=os.environ['META_DB_HOST'], port=os.environ['META_DB_PORT'],
+                            dbname=os.environ['META_DB_NAME'], user=os.environ['META_DB_USER'],
+                            password=os.environ['META_DB_PASSWORD'])
     cur = conn.cursor()
     cur.execute("""SELECT hierarchy FROM meta_variables""")
     metadata = cur.fetchone()[0]
@@ -77,8 +16,9 @@ def get_vars_metadata():
 
 
 def get_vars_names():
-    conn = psycopg2.connect(host=science_db_host, port=science_db_port, dbname=science_db_name, user=science_db_user,
-                            password=science_db_password)
+    conn = psycopg2.connect(host=os.environ['SCIENCE_DB_HOST'], port=os.environ['SCIENCE_DB_PORT'],
+                            dbname=os.environ['SCIENCE_DB_NAME'], user=os.environ['SCIENCE_DB_USER'],
+                            password=os.environ['SCIENCE_DB_PASSWORD'])
     cur = conn.cursor()
     cur.execute("""SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'adni_merge'""")
     names = [t[0] for t in cur.fetchall()]
@@ -87,8 +27,9 @@ def get_vars_names():
 
 
 def get_vars_data():
-    conn = psycopg2.connect(host=science_db_host, port=science_db_port, dbname=science_db_name, user=science_db_user,
-                            password=science_db_password)
+    conn = psycopg2.connect(host=os.environ['SCIENCE_DB_HOST'], port=os.environ['SCIENCE_DB_PORT'],
+                            dbname=os.environ['SCIENCE_DB_NAME'], user=os.environ['SCIENCE_DB_USER'],
+                            password=os.environ['SCIENCE_DB_PASSWORD'])
     cur = conn.cursor()
     cur.execute("""SELECT * FROM adni_merge""")
     data = cur.fetchall()
@@ -96,8 +37,9 @@ def get_vars_data():
 
 
 def save_results(job_id, node, timestamp, pfa, error, shape, function):
-    conn = psycopg2.connect(host=analytics_db_host, port=analytics_db_port, dbname=analytics_db_name,
-                            user=analytics_db_user, password=analytics_db_password)
+    conn = psycopg2.connect(host=os.environ['ANALYTICS_DB_HOST'], port=os.environ['ANALYTICS_DB_PORT'],
+                            dbname=os.environ['ANALYTICS_DB_NAME'], user=os.environ['ANALYTICS_DB_USER'],
+                            password=os.environ['ANALYTICS_DB_PASSWORD'])
     cur = conn.cursor()
     cur.execute("""INSERT INTO job_result VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')"""
                 % (job_id, node, timestamp, pfa, error, shape, function))
