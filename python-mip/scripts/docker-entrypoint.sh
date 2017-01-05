32e3f48f80e09a23a9d9f4a82b220ba1b89774dd
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-01-05 09:32:25 +0100

    update docker-entrypoint compute case

--- python-mip/scripts/docker-entrypoint.sh
+++ python-mip/scripts/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "compute" ]; then
-	mkdir -p "$COMPUTE_IN" "$COMPUTE_OUT"
-	chown -R compute "$COMPUTE_IN" "$COMPUTE_OUT"
+	echo "Running python-histogram computation..."
 fi
