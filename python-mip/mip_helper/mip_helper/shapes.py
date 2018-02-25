8e555501a6fef1b69de348c353d3391055a32d29
Ludovic Claude <ludovic.claude@laposte.net>
2018-02-25 01:10:48 +0100

    Add shapes class to list available shapes

---
+++ python-mip/mip_helper/mip_helper/shapes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Collection of mime types representing the valid shapes to use when saving the result of an algorithm
+"""
+class Shapes:
+      ERROR = "text/plain+error"
+      PFA = "application/pfa+json"
+      PFA_YAML = "application/pfa+yaml"
+      """ Tabular data resource """
+      TABULAR_DATA_RESOURCE = "application/vnd.tabular-data-resource+json"
+      HTML = "text/html"
+      SVG = "image/svg+xml"
+      PNG = "image/png;base64"
+      HIGHCHARTS = "application/vnd.highcharts+json"
+      VISJS = "application/vnd.visjs+javascript"
+      PLOTLY = "application/vnd.plotly.v1+json"
+      VEGA = "application/vnd.vega+json"
+      VEGALITE = "application/vnd.vegalite+json"
+      """ Generic Json, for other types of visualisations """
+      JSON = "application/json"
