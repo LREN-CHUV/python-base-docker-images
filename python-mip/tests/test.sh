22c08a3fd9bfd2002f862245d8b1ba8871603f4f
Marigold <mojmir.vinkler@gmail.com>
2018-03-18 18:42:03 +0100

    bugfix

--- python-mip/tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CHECK=$(docker run --entrypoint=/bin/bash hbpmip/python-mip -c "pip list --format=legacy | grep -c mip-helper")
-
-if [ ${CHECK} -ne 1 ]; then
-    exit 1
-fi
-
-exit 0
